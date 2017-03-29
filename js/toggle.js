31048b43db1b77adfcc2a944e24c0b4ffdd2d638
kovarnyirus <kovarnyi@gmail.com>
2017-03-29 20:19:07 +0300

    начало переверстки меню

---
+++ js/toggle.js
@@ -0,0 +1,17 @@
+/*Мобильное меню*/
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav-toggle');
+
+// navToggle.addEventListener('click', function() {
+//   navMain.classList.contains('main-nav--closed') ?
+//     navMain.classList.remove('main-nav--closed') : navMain.classList.add('main-nav--closed')
+// });
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
