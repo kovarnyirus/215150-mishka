8f70ae50cdb837c56104631c12ad82d2e9f5086d
kovarnyirus <kovarnyi@gmail.com>
2017-04-03 00:26:35 +0300

    no-js правка отступов , исправление ошибки автоматизации

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 /*Мобильное меню*/
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav-toggle');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav-toggle");
+
+navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener("click", function() {
   if (navMain.classList.contains("main-nav--closed")) {
